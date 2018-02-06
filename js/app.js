b5ea22d5d694f6c7ffc798cd0a2dfa257415bd44
Sunny <sunnyksharma1@gmail.com>
2018-02-06 01:14:36 -0500

    Populate parks list, create map

--- js/app.js
+++ js/app.js
@@ -5,8 +5,37 @@
 
 var map;
 
-var parks;
+// list of places
+var parks = [
+  {name: 'Waterton Lakes National Park', location: {lat: 49.0833333, lng: -113.9166667}},
+  {name: 'Pacific Rim National Park Reserve', location: {lat: 48.79193129999999, lng: -125.156359}},
+  {name: 'Yoho National Park', location: {lat: 51.4666667, lng: -116.5833333}},
+  {name: 'Banff National Park', location: {lat: 51.4968464, lng: -115.9280562}},
+  {name: 'Glacier National Park of Canada', location: {lat: 51.335289, lng: -117.5297595}},
+  {name: 'Grasslands National Park', location: {lat: 49.12859760000001, lng: -107.4256163}},
+  {name: 'Writing-on-Stone Provincial Park', location: {lat: 49.0858903, lng: -111.6195612}},
+  {name: 'Bugaboo Provincial Park', location: {lat: 50.8175258, lng: -116.8522904}}
+];
 
+
+
+// callback func
 function initMap() {
 
+var self = this;
+this.filter = ko.observable("");
+this.list = ko.observableArray([]);
+
+map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 6,
+  center: {lat: 51.941496, lng: -122.1828913},
+  mapTypeId: 'satellite'
+});
+// make infoWindows, markers, and eventlisteners w. animation
+
+// add wikipedia links (w. jsonp iirc)
+
+
+// add filter functionality
+// apply bindings
 }
