d68b56c2c736be5c19bcc206ed3d9ddf6c20a315
Sunny <sunnyksharma1@gmail.com>
2018-02-07 19:41:51 -0500

    Add Infowindows

--- js/app.js
+++ js/app.js
@@ -25,8 +25,9 @@ function initMap() {
   this.filter = ko.observable("");
   this.list = ko.observableArray([]);
 
+  // create map
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 6,
+    zoom: 5,
     center: {lat: 51.941496, lng: -122.1828913},
     mapTypeId: 'satellite'
   });
@@ -34,8 +35,8 @@ function initMap() {
   // make infoWindows, markers, and eventlisteners w. animation
   var markers = [];
   var largeInfowindow = new google.maps.InfoWindow();
-  var defaultIcon = makeMarkerIcon('purple');
-  var highlightedIcon = makeMarkerIcon('pink');
+  var defaultIcon = makeMarkerIcon('7f0fe0');
+  var highlightedIcon = makeMarkerIcon('ff00fa');
 
   for (var i = 0; i < parks.length; i++) {
     var position = parks[i].location;
@@ -43,7 +44,8 @@ function initMap() {
     var marker = new google.maps.Marker({
       position: position,
       title: name,
-      animation: google.maps.Animation.BOUNCE???,
+      map: map,
+      animation: google.maps.Animation.DROP,
       icon: defaultIcon
     });
 
@@ -60,16 +62,58 @@ function initMap() {
   }
 
 
-
 // add filter functionality
 // apply bindings
 }
 
 function populateInfoWindow(marker, infowindow) {
-// add wikipedia links (w. jsonp iirc)
 
-};
+if (infowindow.marker != marker) {
+  infowindow.setContent('');
+  infowindow.marker = marker;
+  infowindow.addListener('closeclick', function() {
+    infowindow.marker = null;
+  });
+
+  // add wikipedia links (w. ajax)
+
+
+  //panorama
+  var streetViewService = new google.maps.StreetViewService();
+  var radius = 70;
+
+  function getStreetView(data, status) {
+    if (status == google.maps.StreetViewStatus.OK) {
+      var nearStreetViewLocation = data.location.latLng;
+      var heading = google.maps.geometry.spherical.computeHeading(
+        nearStreetViewLocation, marker.position);
+        infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+        var panoramaOptions = {
+          position: nearStreetViewLocation,
+          pov: {
+            heading: heading,
+            pitch: 31
+          }
+        };
+        var panorama = new google.maps.StreetViewPanorama(
+          document.getElementById('pano'), panoramaOptions);
+        } else {
+          infowindow.setContent('<div>' + marker.title + '</div>' + '<div>No Street View Found</div>');
+        }
+    }
+    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+    infowindow.open(map, marker);
+  }
+}
+
 
 function makeMarkerIcon(markerColor) {
-
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+  return markerImage;
 };
